79f562e745a986db1c82530cfdc7cb83fadee6ab
kaz <kaz.tros@gmail.com>
2021-12-07 17:55:16 -0700

    Moving ChannelAnder class to its own file.  Removing dead includes.

--- Core/Inc/Channel.hpp
+++ Core/Inc/Channel.hpp
@@ -1,7 +1,4 @@
-#include <optional>
-#include "Scheduler.hpp"
-#include "OptionalTuple.hpp"
-
+#include <functional>
 
 template <typename T>
 void source_default (T trash) { /* Do nothing.  Or maybe debug? */ }
@@ -32,48 +29,5 @@ struct ChannelIdentity
 : ChannelProtocol <T>
 {
 	void sink (T x) override { this->source(x); }	
-};
-
-template <typename...T>
-struct ChannelAnder
-: public SourceDelegate <std::tuple <T...>>
-{
-	using TUPLE_T = std::tuple <T...>;
-  using OPT_TUPLE_T = OptionalTuple <T...>;
-
-	template <size_t IDX>
-	using TUPLE_SUB_T = std::tuple_element_t <IDX, TUPLE_T>;
-	
-	
-	OPT_TUPLE_T values_maybe;
-
-
-	ChannelAnder (SourceDelegate <T> & args...)
-	: SourceDelegate <std::tuple <T...>> ()
-	{
-		redirect_sinks_helper (std::std::index_sequence_for <T...> {}, args...);
-	}
-	
-	template <size_t IDX>
-	void sink (TUPLE_SUB_T x) {
-		if ( ! GetIndex (values_maybe.is_values, IDX) ) {
-			std::get <IDX> (values_maybe.values) = x;
-			SetIndex (values_maybe.is_values, IDX);	
-			if (all (values_maybe.is_values)) {
-				this->source (values_maybe.values);
-			}
-		}
-	}
-	
-protected:
-	template <class INT_T, INT_T ... INTS>
-	void redirect_sinks_helper (std::integer_sequence<INT_T, INTS...>, SourceDelegate<T> & args ...) {
-		auto args_as_lref_tuple = std::tie (args...);
-		(	( std::get <INTS> (args_as_lref_tuple).source
-			  = [this] (TUPLE_SUB_T <INTS> x) { this->sink <INTS> (x); }
-			)
-		,
-		...);
-	}
 };
 
---
+++ Core/Inc/ChannelAnder.hpp
@@ -0,0 +1,48 @@
+#include <utility>
+
+#include "Channel.hpp"
+#include "OptionalTuple.hpp"
+
+template <typename...T>
+struct ChannelAnder
+: public SourceDelegate <std::tuple <T...>>
+{
+	using TUPLE_T = std::tuple <T...>;
+  using OPT_TUPLE_T = OptionalTuple <T...>;
+
+	template <size_t IDX>
+	using TUPLE_SUB_T = std::tuple_element_t <IDX, TUPLE_T>;
+	
+	
+	OPT_TUPLE_T values_maybe;
+
+
+	ChannelAnder (SourceDelegate <T> & args...)
+	: SourceDelegate <std::tuple <T...>> ()
+	{
+		redirect_sinks_helper (std::std::index_sequence_for <T...> {}, args...);
+	}
+	
+	template <size_t IDX>
+	void sink (TUPLE_SUB_T x) {
+		if ( ! GetIndex (values_maybe.is_values, IDX) ) {
+			std::get <IDX> (values_maybe.values) = x;
+			SetIndex (values_maybe.is_values, IDX);	
+			if (all (values_maybe.is_values)) {
+				this->source (values_maybe.values);
+			}
+		}
+	}
+	
+protected:
+	template <class INT_T, INT_T ... INTS>
+	void redirect_sinks_helper (std::integer_sequence<INT_T, INTS...>, SourceDelegate<T> & args ...) {
+		auto args_as_lref_tuple = std::tie (args...);
+		(	( std::get <INTS> (args_as_lref_tuple).source
+			  = [this] (TUPLE_SUB_T <INTS> x) { this->sink <INTS> (x); }
+			)
+		,
+		...);
+	}
+};
+
