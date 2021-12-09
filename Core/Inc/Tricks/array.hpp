e9d82d2a18c9ef5ba8cc31e69c9a1c5d5d400bea
kaz <kaz.tros@gmail.com>
2021-12-08 19:07:24 -0700

    rvalues need to be forwarded in array tricks

--- Core/Inc/Tricks/array.hpp
+++ Core/Inc/Tricks/array.hpp
@@ -8,13 +8,19 @@ constexpr std::array <T, N+M> concatenate_helper
   , std::index_sequence <N_INDICES...>
   , std::index_sequence <M_INDICES...>
   )
-{ return std::array <T, N+M> { x[N_INDICES]... , y[M_INDICES]... }; }
+{ return std::array <T, N+M> { std::forward<T>(x[N_INDICES])... , std::forward<T>(y[M_INDICES])... }; }
+
+
+template <typename T, size_t N>
+constexpr auto concatenate (std::array <T, N> && x)
+{   return x;   }
 
 
 template <typename T, size_t N, size_t M, typename ... REST_T>
-constexpr auto concatenate (const std::array <T, N> && x, const std::array <T, M> && y, REST_T && ... rest) {
+constexpr auto concatenate (std::array <T, N> && x, std::array <T, M> && y, REST_T && ... rest) {
   return concatenate
-    ( concatenate_helper (x, y, std::make_index_sequence<N>(), std::make_index_sequence<M>())
-    , rest...
+    ( concatenate_helper (std::forward<std::array <T, N>>(x), std::forward<std::array <T, M>>(y), std::make_index_sequence<N>(), std::make_index_sequence<M>())
+    , std::forward <REST_T> (rest)...
     );
 }
+
